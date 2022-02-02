918bfdd44647efbc726b3eed6ea35b7e9ef5d53c
Nico Wagner <n.wagner@dnb.de>
2022-02-02 16:21:15 +0100

    Add config parameter (#6)

--- src/bin/gnd/skosify.rs
+++ src/bin/gnd/skosify.rs
@@ -1,7 +1,8 @@
+use crate::config::Config;
 use crate::CliResult;
 use std::path::PathBuf;
 
-pub(crate) fn run(_paths: &[PathBuf]) -> CliResult<()> {
-    println!("skosify");
+pub(crate) fn run(_paths: &[PathBuf], config: &Config) -> CliResult<()> {
+    println!("config = {:?}", config);
     Ok(())
 }
