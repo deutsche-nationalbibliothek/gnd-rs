c9f0a136e25104933ac23636f9fe4eff107e1d96
Nico Wagner <n.wagner@dnb.de>
2022-02-02 14:18:05 +0100

    Fix clippy warning

--- src/bin/gnd/skosify.rs
+++ src/bin/gnd/skosify.rs
@@ -1,7 +1,7 @@
 use crate::CliResult;
 use std::path::PathBuf;
 
-pub(crate) fn run(_paths: &Vec<PathBuf>) -> CliResult<()> {
+pub(crate) fn run(_paths: &[PathBuf]) -> CliResult<()> {
     println!("skosify");
     Ok(())
 }
