a988c4e92936accb796d71f98c02a727fec2b036
Nico Wagner <n.wagner@dnb.de>
2022-02-03 08:39:09 +0100

    Add pica reader (#8)

--- src/bin/gnd/skosify.rs
+++ src/bin/gnd/skosify.rs
@@ -1,28 +1,70 @@
 use std::path::PathBuf;
 
 use clap::Parser;
+use pica::matcher::{MatcherFlags, RecordMatcher};
+use pica::ReaderBuilder;
 
 use crate::config::Config;
-use crate::CliResult;
+use crate::{CliError, CliResult};
 
-const DEFAULT_FILTER: &str = "002@.0 =~ '^T[bfgpsu][1z]$'";
+const DEFAULT_FILTER: &str = "002@.0 =~ '^T[bfgpsu][1-7z]$'";
 
 #[derive(Parser, Debug)]
 pub(crate) struct SkosifyArgs {
-    #[clap(long, short)]
+    #[clap(
+        help = "Only include records that match against the filter.",
+        long,
+        short
+    )]
     pub(crate) filter: Option<String>,
+
+    #[clap(
+        help = "Whether to skip invalid PICA+ records or not.",
+        long,
+        short
+    )]
+    pub(crate) skip_invalid: bool,
 
     #[clap(required = false, parse(from_os_str))]
     pub(crate) paths: Vec<PathBuf>,
 }
 
 pub(crate) fn run(config: &Config, args: &SkosifyArgs) -> CliResult<()> {
-    let filter = args
+    let skip_invalid = args.skip_invalid || config.concept.skip_invalid;
+    // TODO: simplify expression via macro?
+    let filter_str = args
         .filter
         .as_deref()
         .or(config.concept.filter.as_deref())
         .unwrap_or(DEFAULT_FILTER);
 
-    println!("filter = {:?}", filter);
+    let filter = match RecordMatcher::new(&filter_str) {
+        Ok(f) => f,
+        Err(_) => {
+            return Err(CliError::Other(format!(
+                "invalid filter: \"{}\"",
+                filter_str
+            )))
+        }
+    };
+
+    // TODO: add matcher flags to config
+    let flags = MatcherFlags::default();
+
+    for filename in &args.paths {
+        let mut reader = ReaderBuilder::new()
+            .skip_invalid(skip_invalid)
+            .from_path(filename)?;
+
+        for result in reader.records() {
+            let record = result?;
+
+            if filter.is_match(&record, &flags) {
+                println!("match!");
+                break;
+            }
+        }
+    }
+
     Ok(())
 }
